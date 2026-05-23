6ee4d2091d5cd10bcbeb3105b2100e26dea875d1
nlam <nlam@f1br1s7.paris.42.school>
2026-05-23 19:26:25 +0200

    FT(cloud-1.sh) add script to launch playbook
    Ensures ansible installed
    Creates inventory.ini based on parameters given
    Launches playbook

---
+++ cloud-1.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -u
+
+# VARIABLES
+INVENTORY="./inventory.ini"
+PLAYBOOK="./playbook.yml"
+
+if [ $# -eq 0 ]; then
+	echo "ERROR: Please enter server IP"
+	exit 1
+fi
+
+# INSTALL: ansible
+if ! command -v ansible >/dev/null 2>&1; then
+    echo "Installing Ansible...."
+	if ! pip install ansible-core; then
+		echo "ERROR: Failed to install Ansible"
+		exit 1
+	fi
+fi
+export PATH=$PATH:~/.local/bin
+echo "Ansible:	$(ansible --version | head -n 1)"
+
+# INSTALL: dependencies
+ansible-galaxy collection install community.crypto community.docker community.general
+
+# CREATE: inventory
+echo "[cloud1hosts]" > "$INVENTORY"
+for arg in "$@"; do
+	echo "$arg" >> "$INVENTORY"
+done
+echo "Inventory:	$INVENTORY"
+
+# CHECK: playbook
+if [ ! -f $PLAYBOOK ]; then
+	echo "ERROR: Playbook file not found"
+	exit 1;
+fi
+echo "Playbook:	$PLAYBOOK"
+
+# RUN
+echo "Running:	ansible-playbook -i $INVENTORY $PLAYBOOK"
+ansible-playbook -i "$INVENTORY" "$PLAYBOOK"
+
+# REMOVE GENERATED INVENTORY FILE
+[ -f "$INVENTORY" ] && rm "$INVENTORY"
+
+trap cleanup EXIT
